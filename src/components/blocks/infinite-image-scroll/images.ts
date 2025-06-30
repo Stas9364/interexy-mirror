1adfeba60b4c59a49497e555ca1ccb0ca1ec24d6
Stas <stas-lisovskii@mail.ru>
2025-06-30 17:27:43 +0300

    create: infinite-image-scroll

---
+++ src/components/blocks/infinite-image-scroll/images.ts
@@ -0,0 +1,22 @@
+export type Image = { src: string; alt?: string; link?: string };
+
+export const images: Image[] = [
+  { src: '/blocks/infinite-image-scroll/gdpr.webp', alt: 'GDPR', link: '' },
+  {
+    src: '/blocks/infinite-image-scroll/blockchain_dev.webp',
+    alt: 'Blockchain Development',
+    link: '',
+  },
+  { src: '/blocks/infinite-image-scroll/clutch_top.webp', alt: 'Clutch Top', link: '' },
+  { src: '/blocks/infinite-image-scroll/hippa.webp', alt: 'HIPAA', link: '' },
+  { src: '/blocks/infinite-image-scroll/mobile_app.webp', alt: 'Mobile App', link: '' },
+  { src: '/blocks/infinite-image-scroll/rf.webp', alt: 'RF', link: '' },
+  { src: '/blocks/infinite-image-scroll/td.webp', alt: 'TD', link: '' },
+  { src: '/blocks/infinite-image-scroll/teachviewer.webp', alt: 'TeachViewer', link: '' },
+  {
+    src: '/blocks/infinite-image-scroll/the_manifest.webp',
+    alt: 'The Manifest',
+    link: '',
+  },
+  { src: '/blocks/infinite-image-scroll/upwork-img.webp', alt: 'Upwork', link: '' },
+];
