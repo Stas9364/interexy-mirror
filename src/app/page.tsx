9c449411d193313172e0540ad278ae9e72ed8a23
Stas <stas-lisovskii@mail.ru>
2025-06-30 12:46:04 +0300

    create: Hero block

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-import { Container } from '@/components/container/Container';
-import { Section } from '@/components/section/Section';
+import { Hero } from '@/components/blocks/hero/Hero';
 
 export default function Home() {
   return (
     <main>
-      <Container>
-        <Section>Section</Section>
-      </Container>
+      <Hero />
     </main>
   );
 }
