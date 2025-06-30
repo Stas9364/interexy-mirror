04b1e436ea6fd0f553c8c4b00c50f7cc6a9150b7
Stas <stas-lisovskii@mail.ru>
2025-06-30 12:42:06 +0300

    replace

--- src/components/container/Container.tsx
+++
@@ -1,5 +0,0 @@
-export const Container = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className='mx-auto w-full max-w-[1690px] px-[15px] xl:px-[80px]'>{children}</div>
-  );
-};
