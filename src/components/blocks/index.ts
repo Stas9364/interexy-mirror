ecd715893c05a3e668cd1e9dd78aee5ad4e3f5a0
Stas <stas-lisovskii@mail.ru>
2025-07-01 11:02:48 +0300

    create: our-success block

---
+++ src/components/blocks/index.ts
@@ -0,0 +1,6 @@
+import { Hero } from './hero/Hero';
+import { InfiniteImageScroll } from './infinite-image-scroll/InfiniteImageScroll';
+import { OurAchievements } from './our-achievements/OurAchievements';
+import { OurSuccess } from './our-success/OurSuccess';
+
+export { Hero, InfiniteImageScroll, OurAchievements, OurSuccess };
---
+++ src/components/layout/index.ts
@@ -0,0 +1,6 @@
+import Header from './header/Header';
+import { Footer } from './footer/Footer';
+import { Section } from './section/Section';
+import { Container } from './container/Container';
+
+export { Header, Footer, Section, Container };
