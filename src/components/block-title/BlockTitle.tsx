ee8cc6b0c69c637d558f4d2003b43e40bcdc96b5
Stas <stas-lisovskii@mail.ru>
2025-07-08 12:18:00 +0300

    fix

--- src/components/block-title/BlockTitle.tsx
+++ src/components/block-title/BlockTitle.tsx
@@ -9,7 +9,7 @@ export const BlockTitle = ({
   children,
 }: {
   title: string;
-  subtitle: string;
+  subtitle?: string;
   className?: string;
   titleClassName?: string;
   subtitleClassName?: string;
@@ -17,14 +17,17 @@ export const BlockTitle = ({
 }) => {
   return (
     <div className={cn('mb-6 flex flex-col gap-y-1 md:mb-10 xl:mb-[60px]', className)}>
-      <p
-        className={cn(
-          'text-accent text-base leading-[140%] font-semibold',
-          subtitleClassName,
-        )}
-      >
-        {subtitle}
-      </p>
+      {subtitle && (
+        <p
+          className={cn(
+            'text-accent text-base leading-[140%] font-semibold',
+            subtitleClassName,
+          )}
+        >
+          {subtitle}
+        </p>
+      )}
+
       <div
         className={cn(
           'text-2xl leading-[140%] font-semibold md:text-4xl xl:text-5xl',
