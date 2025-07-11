b8c566186ed1607130ae643ff910aa8173d869fa
Stas <stas-lisovskii@mail.ru>
2025-07-11 11:00:29 +0300

    add: prettier rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import eslintPluginTs from '@typescript-eslint/eslint-plugin';
+import parserTs from '@typescript-eslint/parser';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +13,18 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    plugins: {
+      '@typescript-eslint': eslintPluginTs,
+      prettier: prettierPlugin,
+    },
+    languageOptions: { parser: parserTs },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'error',
+      'prettier/prettier': ['warn', { endOfLine: 'auto' }],
+    },
+  },
 ];
 
 export default eslintConfig;
