5ee2edbd7667ae2337f1d9226ecd2bcd9e796428
Stas <stas-lisovskii@mail.ru>
2025-08-07 17:59:16 +0300

    fix: position

--- src/components/scroll-to-top/ScrollToTop.tsx
+++ src/components/scroll-to-top/ScrollToTop.tsx
@@ -21,8 +21,8 @@ export const ScrollToTop = () => {
   return (
     <button
       onClick={handleClick}
-      className={`fixed right-4 bottom-8 z-50 flex h-10 w-10 cursor-pointer items-center justify-center border-1 border-[#f5f5f54d] bg-[#434d6766] p-3 text-white shadow-lg backdrop-blur-[4px] transition-opacity duration-300 md:right-8 md:h-[56px] md:w-[56px] ${visible ? 'opacity-100' : 'pointer-events-none opacity-0'} `}
-      aria-label='Наверх'
+      className={`fixed right-5 bottom-[100px] z-50 flex h-10 w-10 cursor-pointer items-center justify-center border-1 border-[#f5f5f54d] bg-[#434d6766] p-3 text-white shadow-lg backdrop-blur-[4px] transition-opacity duration-300 md:right-6 md:h-[56px] md:w-[56px] ${visible ? 'opacity-100' : 'pointer-events-none opacity-0'} `}
+      aria-label='Up'
     >
       <ArrowUp />
     </button>
