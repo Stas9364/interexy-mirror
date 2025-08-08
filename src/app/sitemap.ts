b2cb0e847324ab0511d37f47d8e21d07aac9b1e1
Stas <stas-lisovskii@mail.ru>
2025-08-08 19:06:36 +0300

    fix

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,9 +1,11 @@
 import { MetadataRoute } from 'next';
 
+const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000';
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
-    { url: `https://interexy-mirror.vercel.app/sitemap-static/sitemap.xml` },
-    { url: `https://interexy-mirror.vercel.app/sitemap-blog/sitemap.xml` },
-    { url: `https://interexy-mirror.vercel.app/sitemap-portfolio/sitemap.xml` },
+    { url: `${baseUrl}/sitemap-static/sitemap.xml` },
+    { url: `${baseUrl}/sitemap-blog/sitemap.xml` },
+    { url: `${baseUrl}/sitemap-portfolio/sitemap.xml` },
   ];
 }
