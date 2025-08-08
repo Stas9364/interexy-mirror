3a80d6230bfd956e944dfcde2735faded2f6dcda
Stas <stas-lisovskii@mail.ru>
2025-08-08 22:23:47 +0300

    change: sitemap

---
+++ src/app/sitemap.xml/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000';
+
+  const sitemapIndex = `<?xml version="1.0" encoding="UTF-8"?>
+<sitemapIndex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <sitemap>
+    <loc>${baseUrl}/sitemap-static.xml</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+  </sitemap>
+  <sitemap>
+    <loc>${baseUrl}/sitemap-blog.xml</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+  </sitemap>
+  <sitemap>
+    <loc>${baseUrl}/sitemap-portfolio.xml</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+  </sitemap>
+</sitemapIndex>`;
+
+  return new NextResponse(sitemapIndex, {
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  });
+}
