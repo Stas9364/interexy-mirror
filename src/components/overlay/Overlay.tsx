5e3534ca49ada4aaa6d91f51a363ea871ba69003
Stas <stas-lisovskii@mail.ru>
2025-08-20 15:46:24 +0300

    add: Dockerfile

--- src/components/overlay/Overlay.tsx
+++ src/components/overlay/Overlay.tsx
@@ -1,7 +1,9 @@
+import { cn } from '@/lib/utils';
+
 export const Overlay = ({ className }: { className?: string }) => {
   return (
     <div
-      className={`absolute top-0 left-0 z-1 h-full w-full bg-[#0000004d] ${className}`}
+      className={cn(`absolute top-0 left-0 z-1 h-full w-full bg-[#0000004d]`, className)}
     ></div>
   );
 };
